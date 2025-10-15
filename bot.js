72d82f41ce8ef9dd9327e3ba24b6f4f0b6eb88c4
grinvsevolod1-ctrl <grin.vsevolod1@gmail.com>
2025-10-15 14:36:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,12 @@
+require('dotenv').config();
+const { Telegraf } = require('telegraf');
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
+const topFlood = require('./commands/topFlood');
+const trackVin = require('./commands/trackVin');
+
+bot.start((ctx) => ctx.reply('🚗 Добро пожаловать в AutoSniper!'));
+bot.command('top_flood', topFlood);
+bot.command('track', trackVin);
+
+bot.launch();
